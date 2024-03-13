a1f802d0c1b1d845252d664176330f4ab692584b
GraceMacharia <machariagrace581@gmail.com>
2024-03-13 11:22:37 +0300

    add existing file

---
+++ mpesaapplication/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
+from django_daraja.mpesa.core import MpesaClient
+
+# Create your views here.
+def index(request):
+    cl = MpesaClient()
+    phone_number = '0708374149'
+    amount = 1
+    account_reference = 'reference'
+    transaction_desc = 'Description'
+    callback_url = 'https://darajambili.herokuapp.com/express_payment';
+    response = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
+    return HttpResponse(response)
+def stk_push_callback(request):
+    data = request.body
+    return HttpResponse('stk push in Django')
+
